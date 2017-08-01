6f4db926bce52796982f4cd4609ca7df015b3c29
xiaonanln <xiaonanln@gmail.com>
2017-08-01 17:24:04 +0800

    my version

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
-	"path/filepath"
 	"regexp"
 	"runtime"
 	"sync"
 	"time"
+
+	"path/filepath"
+
+	"github.com/xiaonanln/go-xnsyncutil/xnsyncutil"
 )
 
 const (
@@ -37,37 +41,75 @@ const (
 )
 
 // OutputPath 默认的输出文件路径
-const OutputPath = "treehash.txt"
-const WorkerCount = 100
+const (
+	OutputPath     = "treehash.txt"
+	WorkerCount    = 10000
+	DirWalkerCount = 10000
+)
 
+var waitDirWalkers sync.WaitGroup
 var waitWorkers sync.WaitGroup
 var waitHashWorker sync.WaitGroup
 
-var workQueue = make(chan workItem, WorkerCount)
+var dirWalkQueue = xnsyncutil.NewSyncQueue()
 
-type workItem struct {
+type dirWalkItem struct {
+	Path string
+}
+
+var fileWorkQueue = make(chan fileWorkItem, WorkerCount*10)
+
+type fileWorkItem struct {
 	File string
 	Info os.FileInfo
 }
 
-var hashQueue = make(chan hashItem, 1000)
+var hashWorkQueue = make(chan haskWorkItem, 1000)
 
-type hashItem struct {
+type haskWorkItem struct {
 	File string
 	Hash []byte
 	Info os.FileInfo
 }
 
+func dirWalker(reg *regexp.Regexp) {
+	for {
+		_item := dirWalkQueue.Pop()
+		if _item == nil {
+			break
+		}
+
+		item := _item.(dirWalkItem)
+		println(item.Path)
+		files, _ := ioutil.ReadDir(item.Path)
+		for _, f := range files {
+			fpath := filepath.Join(item.Path, f.Name())
+			if reg.Match([]byte(f.Name())) {
+				continue
+			}
+
+			if f.IsDir() {
+				dirWalkQueue.Push(dirWalkItem{fpath})
+				waitDirWalkers.Add(1)
+			} else {
+				fileWorkQueue <- fileWorkItem{fpath, f}
+			}
+		}
+
+		waitDirWalkers.Done()
+	}
+}
+
 func worker() {
 	buf := make([]byte, 102400)
-	for item := range workQueue {
+	for item := range fileWorkQueue {
 		fd, _ := os.Open(item.File)
 
 		hash := sha1.New()
 		io.CopyBuffer(hash, fd, buf)
 		fd.Close()
 
-		hashQueue <- hashItem{item.File, hash.Sum(nil), item.Info}
+		hashWorkQueue <- haskWorkItem{item.File, hash.Sum(nil), item.Info}
 	}
 
 	waitWorkers.Done()
@@ -77,8 +119,8 @@ func hashwriter(output string) {
 	fd, _ := os.OpenFile(output, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	defer fd.Close()
 
-	for item := range hashQueue {
-		println(item.File)
+	for item := range hashWorkQueue {
+		//println(item.File)
 		fmt.Fprintf(fd, "%s,%x,%d\n", item.File, item.Hash, item.Info.Size())
 	}
 	waitHashWorker.Done()
@@ -116,6 +158,12 @@ func Traverse(rootpath string, filter string, output string) int {
 		}
 	}
 
+	dirWalkQueue.Push(dirWalkItem{rootpath})
+	waitDirWalkers.Add(1)
+	for i := 0; i < DirWalkerCount; i++ {
+		go dirWalker(reg)
+	}
+
 	waitWorkers.Add(WorkerCount)
 	for i := 0; i < WorkerCount; i++ {
 		go worker()
@@ -124,19 +172,20 @@ func Traverse(rootpath string, filter string, output string) int {
 	waitHashWorker.Add(1)
 	go hashwriter(output)
 
-	// 遍历时，保存树中的结点
-	filepath.Walk(rootpath, func(path string, info os.FileInfo, err error) error {
-		if reg.Match([]byte(path)) {
-			return filepath.SkipDir
-		}
+	//// 遍历时，保存树中的结点
+	//filepath.Walk(rootpath, func(path string, info os.FileInfo, err error) error {
+	//	if reg.Match([]byte(path)) {
+	//		return filepath.SkipDir
+	//	}
+	//
+	//	fileWorkQueue <- fileWorkItem{path, info}
+	//	return nil
+	//})
+	waitDirWalkers.Wait()
 
-		workQueue <- workItem{path, info}
-		return nil
-	})
-
-	close(workQueue)
+	close(fileWorkQueue)
 	waitWorkers.Wait()
-	close(hashQueue)
+	close(hashWorkQueue)
 	waitHashWorker.Wait()
 	return Success
 }
